e22d0d4c0190991fe73aa1bcba16a75ac200a44b
camejia <camejia@alum.mit.edu>
2015-04-17 20:27:05 -0700

    Minor tweaks to text

--- PA1.R
+++ PA1.R
@@ -15,46 +15,44 @@ activity <- read.csv("activity.csv", header = TRUE,
 
 # Mean Total Number of Steps Taken per Day --------------------------------
 
-activity1 <- aggregate(steps ~ date, data = activity, FUN = sum)
-
+totalStepsByDate <- aggregate(steps ~ date, data = activity, FUN = sum)
 library(ggplot2)
-qplot(steps, data = activity1, geom = "histogram",
+qplot(steps, data = totalStepsByDate, geom = "histogram",
       main = "Total Number of Steps Taken per Day") 
-mean(activity1$steps)
-median(activity1$steps)
+meanTotalStepsByDate <- mean(totalStepsByDate$steps)
+meanTotalStepsByDate <- median(totalStepsByDate$steps)
 
 # Average Daily Activity Pattern ------------------------------------------
 
-activity2 <- aggregate(steps ~ interval, data = activity, FUN = mean)
-qplot(interval, steps, data = activity2, geom = "line",
+meanStepsByInterval <- aggregate(steps ~ interval, data = activity, FUN = mean)
+qplot(interval, steps, data = meanStepsByInterval, geom = "line",
       main = "Average Number of Steps Taken per Interval") 
-activity2$interval[which.max(activity2$steps)]
+meanStepsByInterval$interval[which.max(meanStepsByInterval$steps)]
 
 # Imputing Missing Values -------------------------------------------------
 
 ok <- complete.cases(activity)
-sum(!ok)
+incompleteCases <- sum(!ok)
 
-activity3 <- merge(activity, activity2, by = "interval")
-activity3$steps.x[is.na(activity3$steps.x)] <- activity3$steps.y[is.na(activity3$steps.x)]
-names(activity3)[names(activity3) == "steps.x"] <- "steps"
-activity3 <- subset(activity3, select = -steps.y)
+imputed <- merge(activity, meanStepsByInterval, by = "interval")
+imputed$steps.x[is.na(imputed$steps.x)] <- imputed$steps.y[is.na(imputed$steps.x)]
+names(imputed)[names(imputed) == "steps.x"] <- "steps"
+imputed <- subset(imputed, select = -steps.y)
 
-activity4 <- aggregate(steps ~ date, data = activity3, FUN = sum)
+impTotalStepsByDate <- aggregate(steps ~ date, data = imputed, FUN = sum)
 
-qplot(steps, data = activity4, geom = "histogram",
+qplot(steps, data = impTotalStepsByDate, geom = "histogram",
       main = "Total Number of Steps Taken per Day\n(with imputed missing values)") 
-mean(activity4$steps)
-median(activity4$steps)
+mean(impTotalStepsByDate$steps)
+median(impTotalStepsByDate$steps)
 
 # Differences in Activity Patterns between Weekdays and Weekends ----------
 
-activity3$daytype <- factor(weekdays(activity3$date) %in% c("Saturday", "Sunday"),
+imputed$daytype <- factor(weekdays(imputed$date) %in% c("Saturday", "Sunday"),
                             labels = c("weekday", "weekend"))
-activity3$daytype <- weekdays(activity3$date) %in% c("Saturday", "Sunday")
 
-activity5 <- aggregate(steps ~ interval + daytype, data = activity3, FUN = mean)
-p <- qplot(interval, steps, data = activity5, geom = "line",
+impMeanStepsByInterval <- aggregate(steps ~ interval + daytype, data = imputed, FUN = mean)
+p <- qplot(interval, steps, data = impMeanStepsByInterval, geom = "line",
       main = "Average Number of Steps Taken per Interval") 
 p + facet_wrap(~ daytype)
 
